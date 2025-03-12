b4f9f1b8134092c232a62b49922f85a737039503
GentaArya <mgentaarya@yahoo.com>
2025-03-12 12:13:58 +0700

    change url

--- src/Services/AxiosInstance.js
+++ src/Services/AxiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://utdrs.apiservices.my.id/api/v1/";
+const API_URL = "https://server-utdrs.vercel.app/api/v1/";
 
 const axiosInstance = axios.create({
   baseURL: API_URL,

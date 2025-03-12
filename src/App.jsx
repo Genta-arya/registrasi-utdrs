7d899614f3c8746d9a85be06dd467e96247ca604
GentaArya <mgentaarya@yahoo.com>
2025-03-12 12:24:59 +0700

    change url

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { Toaster } from "sonner";
-import FormRegister from "./Components/FormRegister";
+import { motion } from "framer-motion";
 import RegisterPage from "./Components/Tabs";
 import Navbar from "./Components/Navbar";
-
 import ScrollToTop from "./Components/ScrollToTop";
 import Footer from "./Components/footer";
+
 
 function App() {
   return (
@@ -12,10 +12,15 @@ function App() {
       {/* Navbar */}
       <Navbar />
 
-      {/* Main Content - Gunakan flex-grow untuk mengisi ruang kosong */}
-      <div className="flex-grow mt-12 lg:mt-16 md:mt-12 flex items-center justify-center bg-white p-4">
+      {/* Main Content - Animasi dengan Framer Motion */}
+      <motion.div
+        className="flex-grow mt-12 lg:mt-16 md:mt-12 flex items-center justify-center bg-white p-4"
+        initial={{ opacity: 0, y: 20 }} // Mulai dari transparan & turun sedikit
+        animate={{ opacity: 1, y: 0 }} // Animasi masuk ke normal
+        transition={{ duration: 0.8, ease: "easeOut" }} // Durasi animasi
+      >
         <RegisterPage />
-      </div>
+      </motion.div>
 
       {/* Toaster Notification */}
       <Toaster position="bottom-center" richColors closeButton duration={3000} />

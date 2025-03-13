0890c4a83829a38c3727b8d7520616cc081dbaec
GentaArya <mgentaarya@yahoo.com>
2025-03-13 12:53:44 +0700

    add midleware

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,27 +1,41 @@
-import React from "react";
-import { PropagateLoader, PulseLoader } from "react-spinners";
+import React, { useState } from "react";
+import ReCAPTCHA from "react-google-recaptcha";
+import { PulseLoader } from "react-spinners";
 
-const Button = ({ children, onClick, text, loading, type , disabled }) => {
+const Button = ({ children, onClick, text, loading, type, disabled }) => {
+  const [recaptchaToken, setRecaptchaToken] = useState(null);
+
+  const handleRecaptchaChange = (value) => {
+    setRecaptchaToken(value); 
+  };
+
   return (
     <div>
       <button
-        className="bg-bata disabled:bg-gray-700 duration-300 disabled:opacity-70 mt-2 font-bold w-full py-2 rounded-lg hover:opacity-80 transition-all  text-white"
+        className="bg-bata disabled:bg-gray-700 duration-300 disabled:opacity-70 mt-2 font-bold w-full py-2 rounded-lg hover:opacity-80 transition-all text-white"
         type={type}
         onClick={onClick}
-        disabled={loading || disabled}
+        disabled={loading || disabled || !recaptchaToken}
       >
         {loading ? (
-          <>
-            <PulseLoader
-              className="flex justify-center items-center mt-1"
-              color="white"
-              size={8}
-            />
-          </>
+          <PulseLoader
+            className="flex justify-center items-center mt-1"
+            color="white"
+            size={8}
+          />
         ) : (
-          <>{text}</>
+          text
         )}
       </button>
+
+      <div className="mb-6 mt-4 text-xs flex justify-center">
+        <ReCAPTCHA
+          type="image"
+          size="normal"
+          sitekey="6Lcv3L4qAAAAAJoWWTTOuo9SubeanyIoNZ2wPKj5"
+          onChange={handleRecaptchaChange}
+        />
+      </div>
     </div>
   );
 };
